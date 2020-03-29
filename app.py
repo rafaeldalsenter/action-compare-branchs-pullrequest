60c6c914d248eca393aaa3b2420b65acf753005f
Rafael Dalsenter <rafaeldalsenter@gmail.com>
2020-03-28 23:25:02 -0300

    Apanhando um pouco das variáveis de ambiente

--- app.py
+++ app.py
@@ -1,34 +1,29 @@
 import os
-import requests
+import sys
+import json
 from github import Github
 
 def main():
+    try:
+        repository = os.environ['GITHUB_REPOSITORY']
+        token = os.environ['GITHUB_TOKEN']
+        event = json.load(os.environ['GITHUB_EVENT_PATH'])
+        number_commits_diff = os.environ['NUMBER_COMMITS_DIFF']
 
-    print("ERRO")
-    return 1
+        repo = Github(token).get_repo(repository)
+        pull_request = repo.get_pull(event["pull_request"]["number"])
+        compare = repo.compare(pull_request.head.ref, pull_request.base.ref)
 
-    # print(f"::set-output name=defaultOutput::Teste de erro")
-    # return 1
-    # try:
-    #     repository = os.environ['GITHUB_REPOSITORY']
-    #     token = os.environ['GITHUB_TOKEN']
-    #     pr_number = os.environ['PR_NUMBER']
-    #     number_commits_diff = os.environ['NUMBER_COMMITS_DIFF']
+        if(compare.total_commits > number_commits_diff):
+            print(f"Há uma diferença entre mais de {number_commits_diff} commits entre o branch Head e Base")
+            sys.exit(1)
 
-    #     repo = Github(token).get_repo(repository)
-    #     pull_request = repo.get_pull(pr_number)
-    #     compare = repo.compare(pull_request.head.ref, pull_request.base.ref)
+        print(f"Branchs Head e Base alinhados!")
+        sys.exit(0)
 
-    #     if(compare.total_commits > number_commits_diff):
-    #         print(f"::set-output name=defaultOutput::Há uma diferença entre mais de {number_commits_diff} commits entre o branch Head e Base")
-    #         return 1
-
-    #     print(f"::set-output name=defaultOutput::Branchs Head e Base alinhados!")
-    #     return 0
-
-    # except Exception as err:
-    #     print(f"::set-output name=defaultOutput::Erro {str(err)}")
-    #     return 1
+    except Exception as err:
+        print(f"Erro {str(err)}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()    
